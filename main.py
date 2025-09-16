4fcae2ec909dd97c9873e5e62aae6f5df38e83cb
daretodream666 <pavel.potorochin@digitaldrugs.tech>
2025-09-16 17:38:06 +0500

    initial

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
-# Является ли строка палиндромом?
+even_array = [2, 4, 6, 8, 9]
+flag = False
 
+for value in even_array:
+    if value % 2 == 1:
+        flag = True
 
-def main():
-    line = input("Enter line: ")
-    left, right = 0, len(line) - 1
-
-    while left < right:
-        while left < right and not line[left].isalnum():
-            left += 1
-        while left < right and not line[right].isalnum():
-            right -= 1
-
-        if line[left].lower() != line[right].lower():
-            return False
-
-        left += 1
-        right -= 1
-
-    return True
-
-
-print(main())
+if flag:
+    print("есть нечётное")
+else:
+    print("все числа чётные")
