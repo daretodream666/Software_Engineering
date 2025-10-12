bed7f0ba1fbf334d7b2308ed4d1f63c0d2a82813
daretodream666 <pavel.potorochin@digitaldrugs.tech>
2025-10-12 19:53:54 +0500

    initial

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-list_1 = [1, 1, 3, 3, 1]
-list_2 = [5, 5, 5, 5, 5, 5, 5]
-list_3 = [2, 2, 1, 2, 2, 5, 6, 7, 1, 3, 2, 2]
+# Напиши программу, которая принимает список оценок студентов по предмету (целые числа от 2 до 5). Нужно определить:
+# Средний балл группы.
+# Количество отличников (оценка 5).
+# Есть ли в списке хотя бы одна двойка.
+
+string = input("enter grades one by one and divide by spacebar: ")
+string = string.split()
+string = list(map(int, string))
 
 
-def change_list(lst: list):
-    lst_set = set(lst)  # {1,3} = [1,1,3,3,1]
-    cool_set = set()  # {}, пустой сет
-    for item in lst_set:  # проходим по каждому в {1,3}
-        iterator = lst.count(item)  # считаем сколько раз встречается 1(3); 3(2)
-        for i in range(
-            1, iterator + 1
-        ):  # господи кто придумал начинать range с 0 И ПРИ ЭТОМ СТОП НАДО ПРОПИСЫВАТЬ +1
-            if i == 1:
-                cool_set.add(item)
-            else:
-                cool_set.add(f"{item}" * i)
-    return cool_set
+def average(lst: list):
+    return sum(lst) / len(lst)
 
 
-print(change_list(list_1))
-print(change_list(list_2))
-print(change_list(list_3))
+def aces(lst: list):
+    return lst.count(5)
+
+
+def anyFs(lst: list):
+    if lst.count(2) == 0:
+        return "No Fs"
+    else:
+        return "There is an F"
+
+
+print(average(string))
+print(aces(string))
+print(anyFs(string))
